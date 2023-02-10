efa9801a0d7ddddc823a0146002dba8d1c3a1127
Object9050 <111750980+Object9050@users.noreply.github.com>
2023-02-10 16:07:53 +0000

    Routen finalisiert mit Promise Logik

--- get-Rid-Of-It/src/express-server/server.js
+++ get-Rid-Of-It/src/express-server/server.js
@@ -41,7 +41,7 @@ app.get('/api/items/:id', (req, res) => {
   jsonFile
     .readJsonFile()
     .then((jsonData) => {
-      let item = jsonData.items.find(item => item.id === itemId);
+      const item = jsonData.items.find(item => item.id === itemId);
       if (!item) {
         return res.status(404).send(`Gegenstand mit ID ${itemId} nicht gefunden`)
       } 
@@ -69,105 +69,75 @@ app.post('/api/items', (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send("Gegenstand konnte nicht hinzugefügt werden");
+      res.status(500).send("Fehler beim Schreiben der Datei");
     });
   });
 
 // Ändert einen Gegenstand
 app.put('/api/items/:id', (req, res) => {
-  let itemId = req.params.id;
-  let itemIndex = jsonData.items.findIndex(item => item.id === itemId);
-  let updatedItem = req.body;
-  if (itemIndex === -1) {
-    res.status(404).send("Gegenstand nicht gefunden")
-  }
-  else {
-    // Festlegen der ID auf bestehende ID, falls geändertes Objekt keine ID enthält.
-    updatedItem.id = jsonData.items[itemIndex].id
-    jsonData.items[itemIndex] = updatedItem;
-    res.status(200).send(updatedItem);
-  }
+  const itemId = req.params.id;
+  const updatedItem = req.body;
+  jsonFile
+    .readJsonFile()
+    .then((jsonData) => {
+      const itemIndex = jsonData.items.findIndex(item => item.id === itemId);
+      if (itemIndex === -1) {
+        return res.status(404).send(`Gegenstand mit ID ${itemId} nicht gefunden`)
+      }
+      else {
+        // Festlegen der ID auf bestehende ID, falls geändertes Objekt keine ID enthält.
+        updatedItem.id = jsonData.items[itemIndex].id
+        jsonData.items[itemIndex] = updatedItem;
+        return jsonFile.writeJsonFile(jsonData);
+      }
+    })
+    .then(() => {
+      res.status(200).send(updatedItem);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Gegenstand mit ID ${itemId} konnte nicht in Datei geschrieben werden`);
+    });
 });
+
 
 // Löscht einen Gegenstand
 app.delete('/api/items/:id', (req, res) => {
-  let itemId = req.params.id;
-  let itemIndex = jsonData.items.findIndex(item => item.id === itemId);
-  if (itemIndex === -1) {
-    res.status(404).send("Gegenstand nicht gefunden");
-  }
-  else {
-    jsonData.items.splice(itemIndex, 1);
-    res.sendStatus(204);
-  }
+  const itemId = req.params.id;
+  jsonFile
+    .readJsonFile()
+    .then((jsonData) => {
+      const itemIndex = jsonData.items.findIndex(item => item.id === itemId);
+      if (itemIndex === -1) {
+        return res.status(404).send(`Gegenstand mit ID ${itemId} nicht gefunden`);
+      }
+      else {
+        jsonData.items.splice(itemIndex, 1);
+        return jsonFile.writeJsonFile(jsonData);
+      }
+    })
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Fehler beim Schreiben der Datei`);
+    });
 });
 
-// Löscht ALLE Gegenstände!
+// Löscht ALLE Gegenstände! 
 app.delete('/api/items', (req, res) => {
-  jsonData.items = [];
-  res.status(204).send("All data deleted.")
+  jsonFile
+    .readJsonFile()
+    .then((jsonData) => {
+      jsonData.items = [];
+      return jsonFile.writeJsonFile(jsonData);
+    })
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Fehler beim Schreiben der Datei`);
+    });
 });
-
-
-/// Daten statisch
-// const data = {
-//   "items": [
-//     {
-//       "id": "1",
-//       "name": "old shirt",
-//       "reasonForRemoval": "outdated",
-//       "photoUrl": "https://example.com/old-shirt.jpg",
-//       "age": 5,
-//       "comments": "",
-//       "dateRemoved": "2022-01-01",
-//       "removalMethod": "donated"
-//     },
-//     {
-//       "id": "2",
-//       "name": "broken lamp",
-//       "reasonForRemoval": "broken",
-//       "photoUrl": "https://example.com/broken-lamp.jpg",
-//       "age": 2,
-//       "comments": "",
-//       "dateRemoved": "2022-02-01",
-//       "removalMethod": "trashed"
-//     },
-//     {
-//       "id": "3",
-//       "name": "worn out shoes",
-//       "reasonForRemoval": "worn out",
-//       "photoUrl": "https://example.com/worn-out-shoes.jpg",
-//       "age": 3,
-//       "comments": "",
-//       "dateRemoved": "2022-03-01",
-//       "removalMethod": "donated"
-//     },
-//     {
-//       "id": "4",
-//       "name": "old phone",
-//       "reasonForRemoval": "upgraded",
-//       "photoUrl": "https://example.com/old-phone.jpg",
-//       "age": 4,
-//       "comments": "",
-//       "dateRemoved": "2022-04-01",
-//       "removalMethod": "recycled"
-//     },
-//     {
-//       "id": "5",
-//       "name": "outdated computer",
-//       "reasonForRemoval": "outdated",
-//       "photoUrl": "https://example.com/outdated-computer.jpg",
-//       "age": 5,
-//       "comments": "",
-//       "dateRemoved": "2022-05-01",
-//       "removalMethod": "sold"
-//     }
-//   ],
-//   "highscore": [
-//     {//macht so noch keinen Sinn
-//       "month": "February",
-//       "date": "2022-12-01",
-//       "score": 100 
-//     }
-//   ]
-// };
