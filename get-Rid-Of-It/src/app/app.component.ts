b50de880ba698eb45bafebe27eecac0c9747fb92
Object9050 <111750980+Object9050@users.noreply.github.com>
2023-02-15 14:05:22 +0000

    code cleanup

--- get-Rid-Of-It/src/app/app.component.ts
+++ get-Rid-Of-It/src/app/app.component.ts
@@ -4,25 +4,8 @@ import { ItemService } from './item.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // template: `
-  //   <ul>
-  //     <li *ngFor="let item of items">{{item.name}}</li>
-  //   </ul>
-  // `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'GET-Rid-Of-It';
 }
-
-
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'GET-Rid-Of-It';
-// }
