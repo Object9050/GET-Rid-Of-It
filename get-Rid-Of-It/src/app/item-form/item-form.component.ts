622e6ee3ad0fe18cc518a3f1292da0dbf0093909
Object9050 <111750980+Object9050@users.noreply.github.com>
2023-02-20 14:54:03 +0000

    Added item-form

--- get-Rid-Of-It/src/app/item-form/item-form.component.ts
+++ get-Rid-Of-It/src/app/item-form/item-form.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Item } from '../item.model.class';
+import { ItemsComponent } from '../items/items.component';
+
 @Component({
   selector: 'app-item-form',
   templateUrl: './item-form.component.html',
@@ -7,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class ItemFormComponent {
 
+  // constructor(private itemsComponent: ItemsComponent){}
+
+  removalMethods = ['Donated', 'Recycled', 'Sold', 'Trashed'];
+
+  // ID = this.itemsComponent.genId();
+  model = new Item('20', 'Dead Plant', 'It lived a short live full of empty watercans',
+   0.2, 'Give me your plants if you hate them', this.removalMethods[1]);
+
+  submitted = false;
+
+  onSubmit(): void { this.submitted = true; }
 }
